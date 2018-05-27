5b518b2b8972c412c62a1d5e9ece49e32c44f6c8
Deepansh Bhargava <deepansh940@gmail.com>
2018-05-27 22:40:24 +0530

    Reduxtagram is up and running. Only styl needs to be fixed

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import CSSTransitionGroup from "react-addons-css-transition-group";
 
 const Photo = React.createClass({
   render() {
@@ -14,6 +15,16 @@ const Photo = React.createClass({
               className="grid-photo"
             />
           </Link>
+
+          <CSSTransitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
           <figcaption>
             <p>{post.caption}</p>
             <div className="control-buttons">
